d0173fe4911c7cf5192e7a1493d5170605584bf7
Pratham632003 <prathamk7569@gmail.com>
2022-08-11 23:34:52 +0530

    Second commit

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const DB_URI = "mongodb://localhost:27017/School-Management?readPreference=prima
 // const DB_URI = "mongodb+srv://Pratham:mongoPratham0987@school.wbl248l.mongodb.net/School?retryWrites=true&w=majority"
 
 const connectToMongo = () =>{
-    mongoose.connect(DB_URI , ()=>{
+    mongoose.connect(process.env.DB_URI , ()=>{
         console.log("Connect to Mongo SuccessFully")
     })
 }
